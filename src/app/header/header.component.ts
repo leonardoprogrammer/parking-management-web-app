6e4f9db0162430e035963dca466dcf97d98901e2
Leonardo Silva <81488877+leonardoprogrammer@users.noreply.github.com>
2025-03-23 21:18:34 -0300

    feat: close top bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -9,14 +9,24 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @ViewChild('menu') menu!: ElementRef;
+
   constructor(private router: Router, private authService: AuthService) {}
 
   navigateTo(route: string) {
     this.router.navigate([route]);
+    this.closeMenu();
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+    this.closeMenu();
+  }
+
+  closeMenu() {
+    if (this.menu) {
+      this.menu.nativeElement.classList.remove('open');
+    }
   }
 }
